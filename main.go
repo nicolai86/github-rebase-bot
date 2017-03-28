96c8fba3df30a57598b06d7b3167477bec38d52a
Raphael Randschau <nicolai86@me.com>
2017-03-28 23:14:54 +0200

    chore(setup): inline hook mgt

--- main.go
+++ main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
 
 	"github.com/google/go-github/github"
@@ -87,4 +89,54 @@ func main() {
 
 	log.Printf("Listening on %q\n", addr)
 	http.ListenAndServe(addr, nil)
+}
+
+func createHook(client *github.Client, publicDNS, owner, repo string) (*github.Hook, error) {
+	hook, _, err := client.Repositories.CreateHook(context.Background(), owner, repo, &github.Hook{
+		Name:   github.String("web"),
+		Active: github.Bool(true),
+		Config: map[string]interface{}{
+			"url":          fmt.Sprintf("%s/events", publicDNS),
+			"content_type": "json",
+		},
+		Events: []string{
+			"pull_request",
+			"status",
+			"pull_request_review",
+			"issues",
+		},
+	})
+	return hook, err
+}
+
+func lookupHook(client *github.Client, publicDNS, owner, repo string) (*github.Hook, error) {
+	hooks, _, err := client.Repositories.ListHooks(context.Background(), owner, repo, &github.ListOptions{})
+	if err != nil {
+		return nil, err
+	}
+
+	var h *github.Hook
+	for _, hook := range hooks {
+		if url, ok := hook.Config["url"].(string); ok {
+			if strings.Contains(url, publicDNS) {
+				h = hook
+			}
+		}
+	}
+	return h, nil
+}
+
+func registerHook(client *github.Client, publicDNS, owner, repo string) (*github.Hook, error) {
+	hook, err := lookupHook(client, publicDNS, owner, repo)
+	if err != nil {
+		return nil, err
+	}
+
+	if hook == nil {
+		hook, err = createHook(client, publicDNS, owner, repo)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return hook, nil
 }
